f22e14377821ca5828c147e0ce4e92b81ac34339
Bhadra424 <70212429+Bhadra424@users.noreply.github.com>
2020-08-25 20:22:29 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -1,52 +1,39 @@
+
 import tweepy
-import logging
-import os
+import os # operating system library
+
 def create_api():
-    CONSUMER_KEY = "ca7JN8RWZTRUMsph2pp52p7ta"
-    CONSUMER_SECRET = "OtATrbPyF06dOxJrtShEky2O2ion57rPvGq0gah6tTGbykeT4k"
-    ACCESS_TOKEN = "1292096828226154501-2zfakwkyYRSSxPlJ7GOZmyL1BLd4Pv"
-    ACCESS_TOKEN_SECRET = "6gyfbmfMW73bBh9VI7eZxulcYu8vVMqPEPg83reQ7eDF7"
+  consumer_key = 'ca7JN8RWZTRUMsph2pp52p7ta'
+  consumer_secret = 'OtATrbPyF06dOxJrtShEky2O2ion57rPvGq0gah6tTGbykeT4k'
+  access_token = '1292096828226154501-PexfzZXy81w31TGSidIrnl2HCWa3mi'
+  access_token_secret = '0ZAAMJUNNHZITN4zn8QMre5ArpeuG8MPAi5gNoAdtbxUG'
 
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth, wait_on_rate_limit=True,
-                     wait_on_rate_limit_notify=True)
-    try:
-        api.verify_credentials()
-    except Exception as e:
-        print('Error creating API', exc_info=True)
-        raise e
-    print('API created')
-    return api
-import tweepy
+  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(access_token, access_token_secret)
+
+  api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+  api.verify_credentials()
+  print('API Created')
+  return api
+  
+# Complete code
 import time
-def emoji_follower_count(user):
-    emoji_numbers = {0: "0️⃣", 1: "1️⃣", 2: "2️⃣", 3: "3️⃣",
-                     4: "4️⃣", 5: "5️⃣", 6: "6️⃣", 7: "7️⃣", 8: "8️⃣", 9: "9️⃣"}
 
-    follower_count_list = [int(i) for i in str(user.followers_count)]
+def follower_count(user):
+  emoji_numbers =  {0: "0️⃣", 1: "1️⃣", 2: "2️⃣", 3: "3️⃣",
+                      4: "4️⃣", 5: "5️⃣", 6: "6️⃣", 7: "7️⃣", 8: "8️⃣", 9: "9️⃣"}
 
-    emoji_followers = ''.join([emoji_numbers[k]
-                               for k in follower_count_list if k in emoji_numbers.keys()])
+  uf_split = [int(i) for i in str(user.followers_count)]# Used to seperate 
 
-    return emoji_followers # FOLLOWERS IN SMILEYS
+  emoji_followers = ''.join([emoji_numbers[j] for j in uf_split if j in emoji_numbers.keys()]) 
+  return emoji_followers
 
+api = create_api()
 
-
-def main():
-    api = create_api()
-
-    while True:
-        # change to your own twitter_handle
-        user = api.get_user('Bmuttika')
-
-        if validate_follower_count(user) == emoji_follower_count(user):
-            printf('you still have the same amount of followers, no update neccesary: {validate_follower_count(user) -> (emoji_follower_count(user)}')
-        else:
-            printf('your amount of followers has changed, updating twitter profile: {validate_follower_count(user) -> {emoji_follower_count(user)}')
-
-            api.update_profile(name=f'veera424|{emoji_follower_count(user)} Followers')
-      
-        print("Waiting to refresh..")
-        time.sleep(60)
+while True:
+    user = api.get_user('Bmuttika')
+    api.update_profile(name=f'Bhadra.muttika|{follower_count(user)} Followers')
+    print(f'Updating Twitter Name : Bhadra.muttika|{follower_count(user)} Followers')
+    print('Waiting to refresh')
+    time.sleep(60)
         
